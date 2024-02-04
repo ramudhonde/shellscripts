ae15f83d200cdf865a691b30537a8c7d8ee168db
root <root@ip-172-31-41-160.ap-south-1.compute.internal>
2024-02-04 09:19:43 +0000

    Adding ram_new from AWS online Console

---
+++ ram_new.sh
@@ -0,0 +1,5 @@
+echo "Hello all, I am Ram and I am creating this ram_new.sh script from AWS online console"
+echo "Just checking its working or not"
+echo "Teaching this Jenkins from Young Minds"
+echo "Young Minds is a very good institute to learn the Devops"
+echo "!! Thank you Young Minds fro this incrediable Knowledge sharing !!"w
